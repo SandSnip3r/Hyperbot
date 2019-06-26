9765064fbb21fead6a087646c3296d3d862caca5
Stone <vistone@c624lmc02tv3zrh.us624.corpintra.net>
2019-06-26 16:36:40 -0700

    added pk2 poc

---
+++ documents/code/pk2-poc/itemData.hpp
@@ -0,0 +1,6 @@
+#ifndef ITEM_DATA_HPP
+#define ITEM_DATA_HPP
+
+class ItemData {};
+
+#endif // ITEM_DATA_HPP
---
+++ documents/code/pk2-poc/skillData.hpp
@@ -0,0 +1,6 @@
+#ifndef SKILL_DATA_HPP
+#define SKILL_DATA_HPP
+
+class SkillData {};
+
+#endif // SKILL_DATA_HPP
