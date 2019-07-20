8b93657d5bb239f379f4d2907a5d09f07f67fef4
SandSnip3r <sandsnip3r@gmail.com>
2019-07-20 16:03:24 -0700

    Now creating a blank config if one doesnt exist

--- Combined/src/phConnector.cpp
+++ Combined/src/phConnector.cpp
@@ -57,8 +57,31 @@
 using namespace std;
 namespace fs = std::experimental::filesystem::v1;
 
+void generateBlankConfig(const fs::path &path) {
+  ofstream outFile(path);
+  if (!outFile) {
+    cerr << "Unable to open new file at path \"" << path << "\"\n";
+  }
+  outFile << "; This is the Hyperbot config file\n";
+  outFile << "\n";
+  outFile << "; Provide the path to the silkroad directory\n";
+  outFile << "[Silkroad]\n";
+  outFile << "path=\n";
+  outFile << "\n";
+  outFile << "; One section is required where the section name is the character name\n";
+  outFile << "; Replace \"CharacterName\" with your character's name\n";
+  outFile << "[CharacterName]\n";
+  outFile << "id=\n";
+  outFile << "password=\n";
+}
+
 int main(int argc, char* argv[]) {
 	const fs::path kConfigFilePath{"config.ini"};
+  if (!fs::exists(kConfigFilePath)) {
+    cerr << "Cannot find config file at path \"" << kConfigFilePath << "\". Generating template at this path. Please open and edit it.\n";
+    generateBlankConfig(kConfigFilePath);
+    return 1;
+  }
   try {
     ini::IniReader configReader{kConfigFilePath};
     config::ConfigData configData(configReader);
@@ -67,7 +90,7 @@ int main(int argc, char* argv[]) {
 	  session.start();
   } catch (std::exception &ex) {
     cerr << ex.what() << '\n';
-    return 1;
+    return 2;
   }
 	return 0;
 }
