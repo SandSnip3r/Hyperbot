d165e51ec3f14c889b4f260b079656d9b2cd1a70
SandSnip3r <sandsnip3r@gmail.com>
2024-05-17 08:30:45 -0700

    Add SkillId and MasteryId

--- include/silkroad_lib/scalar_types.h
+++ include/silkroad_lib/scalar_types.h
@@ -7,6 +7,8 @@ namespace sro::scalar_types {
 
 using EntityGlobalId = uint32_t;
 using ReferenceObjectId = uint32_t;
+using ReferenceSkillId = uint32_t;
+using ReferenceMasteryId = uint32_t;
 using StorageIndexType = uint8_t;
 using OptLevelType = uint8_t;
 
